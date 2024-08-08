ed28a9ffd0498ba57f5b727ba5726bdc21934511
Felipe <felipe.m.f.peixoto@gmail.com>
2024-08-08 17:12:30 -0300

    Modelando dados

--- MeetRio/Model/Models.swift
+++ MeetRio/Model/Models.swift
@@ -7,3 +7,61 @@
 
 import Foundation
 
+// Objetos
+struct Hospede: Codable {
+    var id = UUID()
+    var name: String
+    var country: CountryDetails
+    var picture: Data
+    var hostel: Hostel?
+}
+
+struct Hostel: Codable {
+    var id = UUID()
+    var name: String
+    var email: String
+    var password: String
+    var location: LocationDetails
+}
+
+struct Event: Codable {
+    var id = UUID()
+    var name: String
+    var date: Date
+    var address: AddressDetails
+}
+
+// Relações
+struct FavoriteEvent: Codable {
+    var userID: UUID
+    var eventID: UUID
+}
+
+struct GoingEvent: Codable {
+    var userID: UUID
+    var eventID: UUID
+}
+
+struct RecomendedEvent: Codable {
+    var hostelID: UUID
+    var eventID: UUID
+}
+
+// Componentes
+struct CountryDetails: Codable {
+    var name: String
+    var flag: Data
+}
+
+struct LocationDetails: Codable {
+    var latitude: String
+    var longitude: String
+}
+
+struct AddressDetails: Codable {
+    var street: String
+    var number: String
+    var details: String?
+    var neighborhood: String
+    var postalCode: String
+}
